4424af519aa78342e253c8738217716c843b80b6
locarcio <locarcio@msi-gl65>
2022-01-26 09:59:01 +0100

    First commit

---
+++ src/mainpackage/Santamaria.java
@@ -0,0 +1,12 @@
+package mainpackage;
+
+/**
+ *
+ * @author Bruno
+ */
+public class Santamaria {
+    public static void main(String[] args) {
+        int x = 19 + 4;
+        System.out.println("19 + 4 es " + x);
+    }
+}
